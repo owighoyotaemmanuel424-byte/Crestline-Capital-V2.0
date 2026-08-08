a1c3aaa292553e776f1e2033b2d581aee382fb73
owighoyotaemmanuel424-byte <owighoyotaemmanuel424@gmail.com>
2026-08-08 21:59:44 +0100

    Add responsive cards navigation layout

---
+++ app/cards/layout.tsx
@@ -0,0 +1,2 @@
+import BankingShell from "@/components/BankingShell";
+export default function CardsLayout({ children }: { children: React.ReactNode }) { return <BankingShell>{children}</BankingShell>; }
