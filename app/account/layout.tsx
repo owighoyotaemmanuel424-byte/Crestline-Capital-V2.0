f57a862625c2e5103780a1d65032d1195345fd4d
owighoyotaemmanuel424-byte <owighoyotaemmanuel424@gmail.com>
2026-08-08 21:59:25 +0100

    Add responsive account navigation layout

---
+++ app/account/layout.tsx
@@ -0,0 +1,2 @@
+import BankingShell from "@/components/BankingShell";
+export default function AccountLayout({ children }: { children: React.ReactNode }) { return <BankingShell>{children}</BankingShell>; }
